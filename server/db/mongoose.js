82917ef0570877f7ca3e9b29de77f4c47d1aa719
simon.sim@pathmazing.com <chanvannara1993>
2017-08-22 10:57:02 +0700

    'test'

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -2,7 +2,7 @@ var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
 // console.log(process.env.MONGO_URI);
-mongoose.connect(process.env.MONGO_URI, {
+mongoose.connect(process.env.MONGO_URI || 'mongodb://simon:123456@ds023463.mlab.com:23463/simon-todos', {
     useMongoClient: true
 });
  
--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = process.env.PORT;
+const port = process.env.PORT || 3030;
 
 app.use(bodyParser.json());
 
